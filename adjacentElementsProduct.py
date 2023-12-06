2989f59e386aa3e860e659e2631ece86650946ff
Nurjahon001 <nurjahonbegimqosimova@gmail.com>
2023-12-07 01:46:17 +0300

    def solution(inputArray):

---
+++ adjacentElementsProduct.py
@@ -0,0 +1,10 @@
+def solution(inputArray):
+    length = len(inputArray)
+
+    sum = []
+
+    for i in range(length-1):
+
+        sum.append(inputArray[i] * inputArray[i+1])
+
+    return max(sum)
